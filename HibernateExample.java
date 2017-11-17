8907dfe5ec895341e09c123719c63ac1b6e9e47d
Cassandra Van Lydegraf <cassandra.van.lydegraf@gmail.com>
2017-11-17 16:32:22 -0700

    hibernate

---
+++ HibernateExample.java
@@ -0,0 +1,16 @@
+package hibernateexample;
+
+/**
+ *
+ * @author Cassandra
+ */
+public class HibernateExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ NewHibernateUtil.java
@@ -0,0 +1,34 @@
+package hibernateexample;
+
+
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author Cassandra
+ */
+public class NewHibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            Configuration config = new Configuration();
+            sessionFactory = config.configure("hibernateexample/hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
